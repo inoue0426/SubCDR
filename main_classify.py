2a4674d7e4ab3309a6052f0435030505912a64ba
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2025-02-20 15:47:59 -0500

    add comments

--- main_classify.py
+++ main_classify.py
@@ -36,24 +36,24 @@ def getBinary(Tensors, thresh=-2):
     ones = torch.ones_like(Tensors)
     zeros = torch.zeros_like(Tensors)
     return torch.where(Tensors < thresh, ones, zeros)
-    
+
 #---data batchsize
 def PairFeatures(pairs, drug_subfeat, cline_subfeat, drug_glofeat, cline_glofeat, drug_compo_elem):
-    drug_subs = []; cline_subs = [] 
-    drug_glos = []; cline_glos = [] 
+    drug_subs = []; cline_subs = []
+    drug_glos = []; cline_glos = []
     drug_compos = []; cline_compos = []
     label = []
-    for _, row in pairs.iterrows():   
+    for _, row in pairs.iterrows():
         cline_subs.append(cline_subfeat[str(row[0])])
         drug_subs.append(drug_subfeat[str(row[1])])
         cline_glos.append(np.array(cline_glofeat.loc[row[0]]))
         drug_glos.append(np.array(drug_glofeat.loc[row[1]]))
         drug_compos.append([row[1], drug_compo_elem[str(row[1])]])
         cline_compos.append([row[0], cline_compos_elem])
-        label.append(row[2])    
+        label.append(row[2])
     return drug_subs, cline_subs, drug_glos, cline_glos, drug_compos, cline_compos, label
 
-def BatchGenerate(pairs, drug_subfeat, cline_subfeat, drug_glofeat, cline_glofeat, drug_compo_elem, bs):    
+def BatchGenerate(pairs, drug_subfeat, cline_subfeat, drug_glofeat, cline_glofeat, drug_compo_elem, bs):
     drug_subs, cline_subs, drug_glos, cline_glos, drug_compos, cline_compos, label\
                = PairFeatures(pairs, drug_subfeat, cline_subfeat, drug_glofeat, cline_glofeat, drug_compo_elem)
     ds_loader = Data.DataLoader(BatchData(drug_subs), batch_size=bs, shuffle=False, collate_fn=collate_seq)
@@ -65,9 +65,9 @@ def BatchGenerate(pairs, drug_subfeat, cline_subfeat, drug_glofeat, cline_glofea
 
 def train(drug_loader_train, cline_loader_train, glo_loader_train, label_train):
     loss_train = 0
-    Y_true, Y_pred=[], [] 
+    Y_true, Y_pred=[], []
     for batch, (drug, cline, glo_feat, label) in enumerate(zip(drug_loader_train,\
-                                          cline_loader_train, glo_loader_train, label_train)): 
+                                          cline_loader_train, glo_loader_train, label_train)):
         label = getBinary(label[0])
         pred, _ = model(drug.to(device), cline.to(device), glo_feat.to(device))
         optimizer.zero_grad()
@@ -79,18 +79,18 @@ def train(drug_loader_train, cline_loader_train, glo_loader_train, label_train):
         Y_pred += pred.cpu().detach().numpy().tolist()
     auc, aupr = classification_metric(Y_true, Y_pred)
     print('train-loss=', loss_train/len(CV))
-    print('train-AUC:' + str(round(auc, 4)) + ' train-AUPR:' + str(round(aupr, 4)))   
+    print('train-AUC:' + str(round(auc, 4)) + ' train-AUPR:' + str(round(aupr, 4)))
 
 def test(drug_loader_test, cline_loader_test, glo_loader_test, label_test):
     loss_test = 0
     Y_true, Y_pred=[], []
     all_maps = []
     model.eval()
-    with torch.no_grad():      
+    with torch.no_grad():
         for batch, (drug, cline, glo_feat, label) in enumerate(zip(drug_loader_test,\
-                                                 cline_loader_test, glo_loader_test, label_test)): 
+                                                 cline_loader_test, glo_loader_test, label_test)):
             label = getBinary(label[0])
-            pred, maps = model(drug.to(device), cline.to(device), glo_feat.to(device)) 
+            pred, maps = model(drug.to(device), cline.to(device), glo_feat.to(device))
             loss = myloss(pred, label)
             loss_test += loss.item()
             Y_true += label.cpu().detach().numpy().tolist()
@@ -98,40 +98,47 @@ def test(drug_loader_test, cline_loader_test, glo_loader_test, label_test):
     print('test-loss=', loss.item()/len(Independent))
     auc, aupr = classification_metric(Y_true, Y_pred)
     return auc, aupr, Y_true, Y_pred
-        
+
 #%%---traing and test
 #---Building known matrix
+print('Building known matrix')
 CDR_known = CV.set_index(['Cline', 'Drug']).unstack('Cline')
 CDR_known.columns = CDR_known.columns.droplevel()
-#---MF    
+#---MF
+print('MF')
 CDR_matrix = np.array(CDR_known)
 CDR_mask = 1-np.float32(np.isnan(CDR_matrix))
 CDR_matrix[np.isnan(CDR_matrix)] = 0
+
+print('SVD')
 drug_glofeat, cline_glofeat = svt_solve(A = CDR_matrix, mask = CDR_mask)
 drug_glofeat = pd.DataFrame(drug_glofeat); cline_glofeat = pd.DataFrame(cline_glofeat)
-drug_glofeat.index = list(CDR_known.index); cline_glofeat.index = list(CDR_known.columns)    
+drug_glofeat.index = list(CDR_known.index); cline_glofeat.index = list(CDR_known.columns)
 glo_dim = 2*drug_glofeat.shape[1]
 
 Result = []
 #Randomly shuffle samples
+print('Randomly shuffle samples')
 CV = CV.sample(frac = 1, random_state = seed)
 Independent = Independent.sample(frac = 1, random_state = seed)
- 
+
 batch_sizes = args.bs
+print('batch_sizes = %d'% batch_sizes)
+#---data batchsize
 drug_loader_train, cline_loader_train, glo_loader_train, _, _, label_train = BatchGenerate(CV, \
                    drug_subfeat, cline_subfeat, drug_glofeat, cline_glofeat, drug_compo_elem, bs = batch_sizes)
 drug_loader_test, cline_loader_test, glo_loader_test, dc_test, cc_test, label_test = BatchGenerate(Independent, \
                    drug_subfeat, cline_subfeat, drug_glofeat, cline_glofeat, drug_compo_elem, bs = batch_sizes)
 
- #%%   
+ #%%
 model = SubCDR(SubEncoder(in_drug = drug_dim, in_cline = 8, out = 82), GraphEncoder(in_channels = 32, out_channels = 16), \
-               GloEncoder(in_channels = glo_dim, out_channels = 128), Decoder(in_channels = 160)).to(device)   
+               GloEncoder(in_channels = glo_dim, out_channels = 128), Decoder(in_channels = 160)).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr = args.lr, weight_decay = 1e-4)
 myloss = torch.nn.BCELoss()
 
 #---main
 Result=[]
-start = time.time() 
+start = time.time()
 final_AUC = 0;final_AUPR = 0
 for epoch in range(args.ep):
     print('epoch = %d'% (epoch+1))
@@ -142,9 +149,9 @@ for epoch in range(args.ep):
     if (AUC > final_AUC):
         final_AUC = AUC;final_AUPR = AUPR
         torch.save(model.state_dict(), args.o + "classification_model.pkl")
-        
+
 print('Final_AUC: ' + str(round(final_AUC, 4)) + '  Final_AUPR: ' + str(round(final_AUPR, 4)))
 Result.append([final_AUC, final_AUPR])
-#save_prediction_results  
-odir =  args.o + "classification.txt"  
+#save_prediction_results
+odir =  args.o + "classification.txt"
 np.savetxt(odir, np.array(Result))
